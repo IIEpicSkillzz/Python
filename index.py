ee2f4f626dcccc49033961784172344e315f4c79
Fahim Rashid <fahimrashid790@gmail.com>
2019-07-02 22:04:51 -0700

    da

---
+++ index.py
@@ -0,0 +1,16 @@
+print ('Hello Welcome')
+
+inp = input("Enter your name : ")
+print (inp)
+print ("Hello" + inp)
+
+order = input("What would you like to order today sir : ")
+print (order)
+print ("Is" + order + "correct?")
+
+if input("yes"):
+        print("Right away sir.")
+else:
+        print("Please order again")
+
+
